faea757bde3df04ed161c39e7bc52c3a9ca71df8
chxssun <chxssun9410@gmail.com>
2024-10-14 19:46:44 +0900

    Forced update

--- my-app/components/BackGround.tsx
+++ my-app/components/BackGround.tsx
@@ -1,25 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Image, Dimensions, Text } from 'react-native';
-import Animated, { useSharedValue, useAnimatedStyle, withRepeat, withTiming } from 'react-native-reanimated';
+import { StyleSheet, View, Dimensions, Text } from 'react-native';
+import LottieView from 'lottie-react-native';
 import axios from 'axios';
 
-const { width } = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 
 export default function BackgroundAnimation() {
-  // X 좌표를 나타내는 shared value 생성
-  const translateX = useSharedValue(0);
-
-  // 한문장시
   const [data, setData] = useState({ message: '' });
 
   useEffect(() => {
-    // 애니메이션 설정: 부드럽게 반복 이동
-    translateX.value = withRepeat(
-      withTiming(-width, { duration: 8000 }),
-      -1,
-      true
-    );
-
     const fetchMessage = async () => {
       try {
         const response = await axios.get('http://10.0.2.2:8082/api');
@@ -31,27 +20,19 @@ export default function BackgroundAnimation() {
     fetchMessage();
   }, []);
 
-  // 스타일에 애니메이션 값 적용
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ translateX: translateX.value }],
-    };
-  });
-
   return (
     <View style={styles.container}>
+      <LottieView
+        source={require('../assets/lottie/bamboo.json')} // Lottie 파일 경로를 지정하세요
+        autoPlay
+        loop
+        style={styles.backgroundAnimation}
+      />
       {data.message ? (
         <Text style={styles.subtitle}>{data.message}</Text>
       ) : (
-        <Text>로딩 중...</Text>
+        <Text style={styles.subtitle}>로딩 중...</Text>
       )}
-      <Animated.View style={[styles.background, animatedStyle]}>
-        <Image
-          source={require('../assets/images/react-logo2x.png')}
-          style={styles.image}
-          resizeMode="repeat"
-        />
-      </Animated.View>
     </View>
   );
 }
@@ -59,29 +40,18 @@ export default function BackgroundAnimation() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    overflow: 'hidden',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  background: {
-    flexDirection: 'row',
+  backgroundAnimation: {
     position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-  image: {
-    width: width * 2, // 이미지가 화면을 반복해서 덮도록 두 배로 설정
-    height: '100%',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: 20,
+    width: width,
+    height: height,
   },
   subtitle: {
     fontSize: 18,
     textAlign: 'center',
-    marginTop: 10,
+    color: 'white', // 배경에 따라 텍스트 색상 조정이 필요할 수 있습니다
+    zIndex: 1, // 텍스트를 애니메이션 위에 표시
   },
 });
