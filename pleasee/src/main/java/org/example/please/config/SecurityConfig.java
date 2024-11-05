27014ece56fe1ab0f9bc83a1a07b372506cbb1e0
Yang-jun-yeong <junline3484@naver.com>
2024-11-05 11:54:43 +0900

    오류가 보이지만 데이터베이스 변경이 됨

--- pleasee/src/main/java/org/example/please/config/SecurityConfig.java
+++ pleasee/src/main/java/org/example/please/config/SecurityConfig.java
@@ -14,23 +14,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    // 암호화에요
+    // 암호화 설정
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    // security 비활성화
+    // security 비활성화 및 경로 권한 설정
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // HTTP 보안 설정
         http
                 .csrf(csrf -> csrf.disable()) // CSRF 비활성화
                 .cors(cors -> cors.disable()) // CORS 비활성화, 필요에 따라 커스터마이즈 가능
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**","/uploads/profile/images/**").permitAll() // 특정 경로에 대한 접근 허용
+                        .requestMatchers("/api/**", "/uploads/profile/images/**").permitAll() // 특정 경로에 대한 접근 허용
                         .anyRequest().authenticated() // 다른 모든 요청은 인증 필요
-                        .anyRequest().permitAll() // 모든 요청 허용
                 );
 
         return http.build();
@@ -48,7 +46,4 @@ public class SecurityConfig {
             }
         };
     }
-
-
 }
-
