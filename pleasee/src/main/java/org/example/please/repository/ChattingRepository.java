949d5d2ebd556d3e69d8068d22087a5668bb6267
SJ0503 <didtpwls3857@gmail.com>
2024-11-06 15:24:50 +0900

    채티세션추가 및 user,bot message db insert

--- pleasee/src/main/java/org/example/please/repository/ChattingRepository.java
+++ pleasee/src/main/java/org/example/please/repository/ChattingRepository.java
@@ -2,6 +2,11 @@ package org.example.please.repository;
 
 import org.example.please.entity.Chatting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 public interface ChattingRepository extends JpaRepository<Chatting, Integer> {
+
+    @Query(value = "SELECT * FROM chatting_tb WHERE croom_idx = ?1 ORDER BY created_at DESC LIMIT 1", nativeQuery = true)
+    Chatting findLatestChatByCroomIdx(int croomIdx);
 }
