574bff0afa9778a3115ec1fd9e9acff8a60a4c44
SJ0503 <didtpwls3857@gmail.com>
2024-11-11 17:47:41 +0900

    평가 기능 중

--- my-app/components/Config.tsx
+++ my-app/components/Config.tsx
@@ -1,5 +1,5 @@
 // 기본 IP 주소 설정
-const Addr = "192.168.20.125";  // 필요 시 Add 값을 수정할 수 있음
+const Addr = "192.168.21.165";  // 필요 시 Add 값을 수정할 수 있음
 
 // API 주소 설정
 export const serverAddress = `http://${Addr}:8082`;
