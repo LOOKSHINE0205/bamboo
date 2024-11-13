5ee5ff52acf7b650496c4111893ce2e4dc0b67fb
Jipumpkin <qsoqso24@gmail.com>
2024-11-13 09:54:53 +0900

    랭체인 정상화

--- emotion_classification_api/openai_service.py
+++ emotion_classification_api/openai_service.py
@@ -1,24 +1,15 @@
 # openai_service.py
+
 import os
 from dotenv import load_dotenv
-from langchain_openai.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-llm = ChatOpenAI(model_name="gpt-4o-mini-2024-07-18", temperature=0.7, openai_api_key=OPENAI_API_KEY)
-
-
-def generate_gpt_response(messages: list) -> str:
-    """
-    OpenAI API를 사용하여 GPT 모델의 응답을 생성합니다.
-    
-    Args:
-        messages (list): OpenAI API에 전달할 메시지 리스트
-    
-    Returns:
-        str: 생성된 챗봇의 응답
-    """
-    response = llm(messages)
-    return response["choices"][0]["message"]["content"]
+llm = ChatOpenAI(
+    model_name="gpt-4o-mini-2024-07-18",
+    temperature=0.7,
+    openai_api_key=OPENAI_API_KEY
+)
